d23440a512d10c08c1ac64268b99dfadee62d8d2
Mora <2709080+moracoderz@users.noreply.github.com>
2022-05-02 17:21:54 -0500

    Add files via upload

---
+++ main_mods.js
@@ -0,0 +1,114 @@
+function validateNombreCli(auxNombreCli) {
+    if (auxNombreCli === "" || auxNombreCli === null) {
+        alert("Ingrese nombre del cliente");
+        return false;
+    } else {
+        return auxNombreCli.toLowerCase();
+    }
+}
+
+function eleccionPlan() {
+    let ulSubPlanes = document.querySelector("#subPlans-list");
+    ulSubPlanes.classList.remove("subPlanHide");
+}
+
+function getPlanSubPlanPrice(plan, subPlan) {
+    
+    let precioSubPlan = 0;
+    let precioPlan = 0;
+
+    for (let k = 0; k < planes.length; k++) {
+        if (planes[k].idPlan === plan) {
+            for (let l = 0; l < planes[k].subPlanes.length; l++) {
+                if (planes[k].subPlanes[l].idSub === subPlan) {
+                    precioPlan = planes[k].precio;
+                    precioSubPlan = planes[k].subPlanes[l].precio;
+                }
+            }
+        }
+    }
+    return (precioPlan + precioSubPlan);
+}
+
+
+
+
+const agregarCliente = () => {    
+    const seccionClientes = document.querySelector("#mostrarClientes");
+    seccionClientes.style.display = "block";
+    const ul = document.createElement("ul");
+    const li = document.createElement("li");
+    let plan = "";
+    let subPlan = "";
+    let total = 0;
+
+    let nombreCli = validateNombreCli(document.querySelector("#nombreCliente").value);
+
+    const seleccionarPlanes = document.querySelector("#agregarCliente");
+    for (i of seleccionarPlanes.plan) {
+        if (i.checked) {
+            plan = parseInt(i.value);           
+        }
+    }
+
+    const seleccionarSubPlanes = document.querySelector("#agregarCliente");
+    for (j of seleccionarSubPlanes.subPlan) {
+        if (j.checked) {
+            subPlan = parseInt(j.value);           
+        }
+    }
+
+
+    const obtenerPrecioPlan = (plan) => {
+    planes.map(n => {
+        if (n.idPlan === plan) { 
+        return (n.precio) }  else { 
+            null } 
+        });
+}
+
+const obtenerPrecioSubPlan = (subPlan) => {
+    planes.forEach(n => {
+        
+        n.subPlanes.map(subPlanes => {
+            subPlan === subPlanes.idSub ? console.log(subPlanes.precio) : null })
+    });
+}
+
+const obtenerTotal = (obtenerPrecioPlan,obtenerPrecioSubPlan) => {
+    console.log(obtenerPrecioPlan);
+}
+
+
+
+    //total = getPlanSubPlanPrice(plan, subPlan);
+    obtenerTotal(obtenerPrecioPlan(plan),obtenerPrecioPlan(subPlan));
+
+    li.textContent = `Nombre: ${nombreCli} \nPlan: ${plan} \nSub Plan: ${subPlan} \nTotal: ${total}`;
+    
+    clientes.push(new Cliente(nombreCli, plan, subPlan, total));
+    console.log(new Cliente(nombreCli, plan, subPlan, total));
+
+    // Agregando los clientes a la lista en el DOM
+    seccionClientes.appendChild(ul);
+    ul.appendChild(li);
+    li.appendChild(document.createElement("hr"));
+
+    // Limpiar los campos   
+    document.querySelector("#nombreCliente").value = "";
+    document.querySelector("#agregarCliente").reset();
+    document.querySelector("#subPlans-list").classList.add("subPlanHide");
+
+}
+
+const loadEvents = () => {
+    document.querySelector("#agregarClienteBtn").addEventListener("click", agregarCliente);
+
+    const clickPlan = document.querySelectorAll("input[name='plan']");
+    for (i of clickPlan) {
+            i.onchange = () => { eleccionPlan(); }
+    
+        
+    }
+
+}
